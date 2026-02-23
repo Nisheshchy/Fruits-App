7bad415357acfaf94edf26b78a9b45d110c3d202
Nisheshchy <nisheshchy2022@gmail.com>
2026-02-23 22:30:22 +0545

    update and add images and some fix errors

---
+++ src/utility/menu.js
@@ -0,0 +1,27 @@
+export const NavbarMenu = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/",
+    },
+    {
+        id: 2,
+        name: "Product",
+        link: "#",
+    },
+    {
+        id: 3,
+        name: "About",
+        link: "#",
+    },
+    {
+        id: 4,
+        name: "Shop",
+        link: "#",
+    },
+    {
+        id: 5,
+        name: "Contact",
+        link: "#",
+    },
+];
